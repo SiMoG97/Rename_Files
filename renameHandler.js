e988c5cbd8a17a6568efc6580dff09eeed3385c6
Echaarani Mohamed <echaaranimohamed@gmail.com>
2020-08-21 01:34:59 +0100

    adding try catch block

--- renameHandler.js
+++ renameHandler.js
@@ -5,21 +5,24 @@ const renameHandler = (newName, dirPath, ext) => {
   if (ext[0] !== ".") {
     ext = `.${ext}`;
   }
-
-  fs.readdirSync(dirPath)
-    .filter((file) => {
-      return path.extname(file) === ext;
-    })
-    .forEach((file, i) => {
-      fs.rename(
-        `${dirPath}\\${file}`,
-        `${dirPath}\\${newName} ${i + 1}${ext}`,
-        (err) => {
-          if (err) throw err;
-          console.log("Files renamed successfully");
-        }
-      );
-    });
+  try {
+    fs.readdirSync(dirPath)
+      .filter((file) => {
+        return path.extname(file) === ext;
+      })
+      .forEach((file, i) => {
+        fs.rename(
+          `${dirPath}\\${file}`,
+          `${dirPath}\\${newName} ${i + 1}${ext}`,
+          (err) => {
+            if (err) throw err;
+            console.log("Files renamed successfully");
+          }
+        );
+      });
+  } catch (e) {
+    console.log("no such file or directory");
+  }
 };
 
 module.exports = {
